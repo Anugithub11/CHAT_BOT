b8a73f6aa88f73c129857dc857daa08c79a63aa5
Anugithub11 <114275507+Anugithub11@users.noreply.github.com>
2022-10-14 23:28:07 +0530

    Delete publish.cmd

--- publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\anuhyachatbot-bot-ba6e-Web-Deploy.pubxml -p:Password=jGuiQGlSbZzEMwM3DBrqZl4iyJtLhZYRjpjCEA1LtPQj27t0ga9krAQF7Tg7
-
