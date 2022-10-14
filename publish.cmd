a8b11277c51f045fe53e66ba32de109c7906529d
Anugithub11 <114275507+Anugithub11@users.noreply.github.com>
2022-10-14 23:24:23 +0530

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\anuhyachatbot-bot-ba6e-Web-Deploy.pubxml -p:Password=jGuiQGlSbZzEMwM3DBrqZl4iyJtLhZYRjpjCEA1LtPQj27t0ga9krAQF7Tg7
+
